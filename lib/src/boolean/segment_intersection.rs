cfca4fe4fb6815be8e96715736d7e996b30ff991
Nick Parker <nmp67@cornell.edu>
2020-05-16 14:51:17 -0700

    Separate two versions of test_intersection_order
    Added separate tests to check whether intersection order affects the
    type of intersection found or only the coordinates of the intersection.

--- lib/src/boolean/segment_intersection.rs
+++ lib/src/boolean/segment_intersection.rs
@@ -451,22 +451,54 @@ mod test {
     }
 
     #[test]
-    fn test_intersection_order() {
+    fn test_intersection_order_same_value() {
+        fn check_all_combinations(a1: Coordinate<f32>,
+                                  a2: Coordinate<f32>,
+                                  b1: Coordinate<f32>,
+                                  b2: Coordinate<f32>) {
+            let p1234 = intersection(a1, a2, b1, b2);
+            assert_eq!(p1234, intersection(a2, a1, b1, b2));
+            assert_eq!(p1234, intersection(a1, a2, b2, b1));
+            assert_eq!(p1234, intersection(a2, a1, b2, b1));
+            assert_eq!(p1234, intersection(b1, b2, a1, a2));
+            assert_eq!(p1234, intersection(b1, b2, a2, a1));
+            assert_eq!(p1234, intersection(b2, b1, a1, a2));
+            assert_eq!(p1234, intersection(b2, b1, a2, a1));
+        }
+
+        for _ in 0..100000 {
+            let a1 = random_coord();
+            let a2 = random_coord();
+            let b1 = random_coord();
+            let b2 = random_coord();
+            let bm = Coordinate{x: (b1.x + b2.x)/2., y: (b1.y + b2.y)/2.};
+            check_all_combinations(a1, a2, b1, b2);
+            check_all_combinations(a1, a2, a1, b2);
+            check_all_combinations(a1, bm, b1, b2);
+            }
+    }
+
+    #[test]
+    fn test_intersection_order_same_variant() {
 
         fn check_same_variant(i1: LineIntersection<f32>, i2: LineIntersection<f32>) -> bool{
             use LineIntersection::*;
-            match (i1, i2) {
+            let same = match (i1, i2) {
                 (LineIntersection::None, LineIntersection::None) => true,
                 (Point(_a), Point(_b)) => true,
                 (Overlap(_a, _b), Overlap(_c, _d)) => true,
                 _ => false
+            };
+            if !same {
+                println!("{:?} differs from {:?} with coords:", i1, i2);
             }
+            same
         }
 
         fn check_all_combinations(a1: Coordinate<f32>,
                                   a2: Coordinate<f32>,
-                                  b1: Coordinate<f32>, b2:
-                                  Coordinate<f32>) {
+                                  b1: Coordinate<f32>,
+                                  b2: Coordinate<f32>) {
             let p1234 = intersection(a1, a2, b1, b2);
             assert!(check_same_variant(p1234, intersection(a2, a1, b1, b2)));
             assert!(check_same_variant(p1234, intersection(a1, a2, b2, b1)));
@@ -477,15 +509,18 @@ mod test {
             assert!(check_same_variant(p1234, intersection(b2, b1, a2, a1)));
         }
 
-        for _ in 0..1000000 {
+        for _ in 0..100000 {
             let a1 = random_coord();
             let a2 = random_coord();
             let b1 = random_coord();
             let b2 = random_coord();
             let bm = Coordinate{x: (b1.x + b2.x)/2., y: (b1.y + b2.y)/2.};
-            check_all_combinations(a1, a2, b1, b2);
+            // Shared endpoint
             check_all_combinations(a1, a2, a1, b2);
+            // Endpoint of one in interior of other
             check_all_combinations(a1, bm, b1, b2);
+            // Colinear shared endpoint
+            check_all_combinations(b1, bm, bm, b2);
             }
     }
 }
